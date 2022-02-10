7c79f2aeee431a3da88422d65155d86421181442
harshhshukla <harsh.0268@tpoly.in>
2022-02-10 20:31:32 +0530

    signin,cart,mens,womens,kids,contact

--- Ecommerce/Home/urls.py
+++ Ecommerce/Home/urls.py
@@ -3,6 +3,14 @@ from .import views
 from django.urls import path
 urlpatterns = [
     path('',views.Home1),
-    path('login/',views.Login)
+    path('login/',views.Login),
+    path('mens/',views.Mens),
+    path('womens/',views.Womens),
+    path('cart/',views.Cart),
+    path('signin/',views.Signin),
+    path('kids/',views.Kids),
+    path('contact/',views.ContactUs)
+   
+  
 ]
 
--- Ecommerce/Home/views.py
+++ Ecommerce/Home/views.py
@@ -9,5 +9,29 @@ def Home1(request):
 def Login(request):
     return render(request,"homepage/login.html")
 
+def Mens(request):
+    return render(request,"homepage/mens.html")
+
+
+def Womens(request):
+    return render(request,"homepage/womens.html")
+
+def Cart(request):
+    return render(request,"homepage/cart.html")
+
+def Signin(request):
+    return render(request,"homepage/signin.html")
+
+
+
+def Kids(request):
+    return render(request,"homepage/kids.html")
+
+
+def ContactUs(request):
+    return render(request,"homepage/contact.html")
+
+
+
 
     
