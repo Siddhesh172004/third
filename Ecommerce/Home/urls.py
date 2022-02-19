d9ea7df04d2d7d541aba5a6e37b5a432f5dd54a0
Siddhesh172004 <jondhale172004@gmail.com>
2022-02-19 15:10:05 +0530

    sigin and login added

--- Ecommerce/Home/urls.py
+++ Ecommerce/Home/urls.py
@@ -3,13 +3,16 @@ from .import views
 from django.urls import path
 urlpatterns = [
     path('',views.Home1),
-    path('login/',views.Login),
     path('mens/',views.Mens),
     path('womens/',views.Womens),
     path('cart/',views.Cart),
-    path('signin/',views.Signin),
     path('kids/',views.Kids),
-    path('contact/',views.ContactUs)
+    path('contact/',views.ContactUs),
+
+    path('signin/',views.Signin),
+    path('login/',views.Login),
+    path('signindo/',views.Signindo),
+    path('logindo/',views.Logindo),
     
   
 ]
--- Ecommerce/Home/views.py
+++ Ecommerce/Home/views.py
@@ -1,18 +1,85 @@
 from urllib import request
-from django.shortcuts import render
-
-from django.http import HttpResponse
-from . models import Mens
-
+from django.shortcuts import render,redirect
+from django.http.response import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login ,logout
 # Create your views here.
 def Home1(request):
     return render(request,"homepage/index.html")
+
+def Signin(request):
+    return render(request,"homepage/signin.html")
+
+def Signindo(request):
+    if request.method == "POST":
+        username = request.POST.get("email")
+        email= username
+        pass1= request.POST.get("password")
+        pass2= request.POST.get("confirm_password")
+        first_name = request.POST.get("first_name")
+        last_name= request.POST.get("last_name")
+        
+        # validationssssss
+        if (User.objects.filter(username=username).exists()): 
+            # messages.add_message(request, messages.INFO, 'Username already exist.')
+            return redirect("/signin/")           
+            
+
+        if(not(len(pass1)>7 and not pass1.isalnum())):
+            # messages.add_message(request, messages.INFO, 'Password must belonger then 8 characters and should contain a symbol.')
+            return redirect("/signin/")
+        
+        if(not(pass1==pass2)):
+            # messages.add_message(request, messages.INFO, 'Both the password must be same.')
+            return redirect("/signin/")
+
+        else:
+            try:
+                newUser = User.objects.create_user(username,email,pass1)
+                newUser.first_name = first_name
+                newUser.last_name = last_name
+                newUser.save()
+                # messages.add_message(request, messages.SUCCESS, 'Account succsessfully Created. Please Login to continue.')
+                return redirect("/login/")
+            except Exception as e:
+                # messages.add_message(request, messages.ERROR, e)
+                return redirect("/signin/")            
+    else:
+        # messages.add_message(request, messages.ERROR, 'Please SignIn.')
+        return redirect("/login/")
+
 
 
 def Login(request):
     return render(request,"homepage/login.html")
 
+def Logindo(request):
+    if request.method == "POST":
+        username = request.POST.get("email")
+        password = request.POST.get("password")
+        
+        user = authenticate(username = username , password = password )
+
+        if user is not None:
+            login(request,user)
+            # messages.add_message(request, messages.SUCCESS, 'LOGIN successfull!!')
+            return redirect("/")
+
+        else:
+            # messages.add_message(request, messages.ERROR, 'Email or password is not valid. ')
+            return redirect("/login/")
+    else:
+        # messages.add_message(request, messages.ERROR, 'Please Login.')
+        return redirect("/login/")
+    
+
+
+
+
 def Mens(request):
+
+    
     return render(request,"homepage/mens.html")
 
 
@@ -21,9 +88,6 @@ def Womens(request):
 
 def Cart(request):
     return render(request,"homepage/cart.html")
-
-def Signin(request):
-    return render(request,"homepage/signin.html")
 
 
 
@@ -35,11 +99,6 @@ def ContactUs(request):
     return render(request,"homepage/contact.html")
 
 
-def List(request):
-    params = {
-        "list" : [1,2,3,4,5,6,7]
-    }
-    
-    return render(request,"homepage\Mens.html",params)
+
 
     
